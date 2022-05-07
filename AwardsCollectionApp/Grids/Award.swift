d7ca5ab5f9e74f2fffffc252fb3bd174c2d42674
rhiskey <first3395@gmail.com>
2022-05-07 22:00:50 +0300

    Initial Commit

---
+++ AwardsCollectionApp/Grids/Award.swift
@@ -0,0 +1,42 @@
+//
+//  Aword.swift
+//  AwardsCollectionApp
+//
+//  Created by brubru on 07.03.2022.
+//
+
+import SwiftUI
+
+struct Award {
+    
+    let awardView: AnyView
+    let title: String
+    let awarded: Bool
+    
+    static func getAwards() -> [Award] {
+        [
+            Award(
+                awardView: AnyView(GradientRectangles().frame(width: 160, height: 160)),
+                title: "Gradient Rectangles",
+                awarded: true
+            ),
+            Award(
+                awardView: AnyView(PathView().frame(width: 160, height: 160)),
+                title: "Path View",
+                awarded: true
+            ),
+            Award(
+                awardView: AnyView(CurvesView().frame(width: 160, height: 160)),
+                title: "Curves View",
+                awarded: true
+            ),
+            Award(
+                awardView: AnyView(HypocycloidView(width: 160, height: 160)),
+                title: "Hypocycloid View",
+                awarded: false
+            )
+        ]
+    }
+
+}
+
